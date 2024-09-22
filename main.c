f5e5d81b5699da0a14ce5d5a3da80c8993a190bf
Altynaim6 <151496795+Altynaim6@users.noreply.github.com>
2024-09-22 21:09:19 +0600

    first commit

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    double num = 10.7643789;  
+    int intPart = (int)num;  
+    double fracPart = num - intPart;
+
+    printf("Integer Part: ");
+    toBinary(intPart);  
+    printf("\n");
+
+    printf("Fruction Part: ");
+    ExtraOne(fracPart, 8); 
+    printf("\n");
+
+    return 0;
+}
+
+
+
